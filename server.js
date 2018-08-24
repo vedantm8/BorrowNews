a4eb965e68c6629acf7136e6d872bb221668f28e
vedantm8 <vedantm8@gmail.com>
2018-08-23 20:08:29 -0400

    fixing

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var db = require("./models");
 
 var PORT = 3000 || process.env.PORT;
 
-mongoose.connect("localhost/week18Populater");
+mongoose.connect("mongodb://localhost/week18Populater");
 
 // Initialize Express
 var app = express();
